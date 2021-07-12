98266ab9e0120e3a1cfec151d26553b9e38a7240
honor2016tw <honor2016tw@gmail.com>
2021-07-12 17:03:15 +0800

    7/12

---
+++ ZeroJudge/d294-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int Combi(int n){
+  return n*(n+1)/2;
+}
+
+int main(){
+  int n,m;
+  while(cin>>n>>m){
+      cout<<Combi(n)*Combi(m)<<endl;
+    }
+}
