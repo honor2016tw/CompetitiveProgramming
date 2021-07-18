6a34ccc78a5d16c1de44e8a96c76f50252b4a8d3
honor2016tw <honor2016tw@gmail.com>
2021-07-18 14:38:01 +0800

    7/18-2

---
+++ ZeroJudge/d096.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll int N;
+    while(cin>>N){
+        ll int n=(N-1)/2+1;
+        cout<<3*(2*n*n-1)-6<<endl;
+      }
+  }
