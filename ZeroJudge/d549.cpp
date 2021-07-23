7a93979d21018f9c1f6699324a6b19fc2a2f6c21
honor2016tw <honor2016tw@gmail.com>
2021-07-24 06:02:28 +0800

    7/24

---
+++ ZeroJudge/d549.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+  double a,b,c;
+  while(cin>>a>>b>>c)cout<<setprecision(2)<<fixed<<sqrt(a*a+c*c-b*b)<<endl;
+}
