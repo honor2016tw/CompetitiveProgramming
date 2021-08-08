cb406f1e95397051f0888aa91c5f24ee1551b975
honor2016tw <honor2016tw@gmail.com>
2021-08-08 22:24:27 +0000

    8/9-1

--- ZeroJudge/a006.cpp
+++ ZeroJudge/a006.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 int main(){
 	int a,b,c;
-	cin>>a>>b>>c;
-	int x1=(-1*b+sqrt(b*b-4*a*c))/(2*a);
-	int x2=(-1*b-sqrt(b*b-4*a*c))/(2*a);	
-	if(b*b-4*a*c>=0){
-		if(x1==x2) cout<<"Two same roots x="<<x1<<endl;
-		else cout<<"Two different roots x1="<<x1<<" , x2="<<x2<<endl;
-	}else{
-		cout<<"No real root"<<endl;
+	while(cin>>a>>b>>c){
+		int x1=(-1*b+sqrt(b*b-4*a*c))/(2*a);
+		int x2=(-1*b-sqrt(b*b-4*a*c))/(2*a);	
+		if(b*b-4*a*c>=0){
+			if(x1==x2) cout<<"Two same roots x="<<x1<<endl;
+			else cout<<"Two different roots x1="<<x1<<" , x2="<<x2<<endl;
+		}else cout<<"No real root"<<endl;
 	}
 }	
--- ZeroJudge/a009.cpp
+++ ZeroJudge/a009.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
 	string T;
 	while(cin>>T){
-	    for(int i=0;i<T.length();i++){
+	  for(int i=0;i<T.length();i++){
 		T[i]-=7;
 		cout<<T[i];
-	    }
+	 }
 	}
 }
