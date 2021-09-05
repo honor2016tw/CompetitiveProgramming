1cbf1c83ab278424dce3d2236bc4b2efe0e6a71f
honor2016tw <honor2016tw@gmail.com>
2021-09-05 13:59:46 +0800

    9/5

---
+++ GreenJudge/a030-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define abs(a) (a<0?(-a):a)
+
+using namespace std;
+
+int main(){
+  int a,b;
+  while(cin>>a>>b){
+    int sum=0;
+    for(int i=a,j=0;j<(abs((a-b))+1);i--,j++){
+      cout<<(a>b?i:(a+j));
+      if(j!=abs((a-b))) cout <<"+";
+      sum+=(a>b?i:(a+j));
+    }
+    cout<<"="<<sum<<endl;
+  }
+}
+
--- GreenJudge/d062.cpp
+++ GreenJudge/d062.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
-	
+	int N,len[4];
+	while(cin>>N){
+		while(N--){
+			int equal=0;
+			for(int i=0;i<=3;i++) cin>>len[i];
+
+		}
+	}
 }
