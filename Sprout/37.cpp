59df6cfa3a8389ebfe8b814e5fdfb7abb3eef1b8
honor2016tw <honor2016tw@gmail.com>
2021-09-12 08:34:40 +0800

    9/12

---
+++ Sprout/37.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+  queue<int> Q;
+  int T;
+
+  while(cin>>T){
+    while(T--){
+      int a,b;
+      while(cin>>a){
+        switch(a){
+          case 1:
+            cin>>b;
+            Q.push(b);
+            break;
+          case 2:
+            if(Q.empty()){
+              cout<<"empty!"<<endl;
+              break;
+            }
+            cout<<Q.front()<<endl;
+            Q.pop();
+            break;
+        }
+      }
+    }
+  }
+}
