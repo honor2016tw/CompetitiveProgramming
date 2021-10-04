f53ae39440a6b93dbcb2926df132a00cd7534cc9
honor2016tw <honor2016tw@gmail.com>
2021-10-04 11:15:49 +0800

    10/4

---
+++ GreenJudge/b037.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int ans[100];
+
+void permute(int idx,int N,int M){
+    if(idx==N){
+        for(int i=0;i<N;i++){
+            cout<<ans[i];
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i=0;i<=M;i++){ 
+        ans[idx]=i;
+        permute(idx+1,N,M);
+    }
+}
+
+int main(){
+    int N,M;
+    while(cin>>N>>M){
+       permute(0,N,M); 
+    }
+}
