0e4440d7ed625323fa212a65b06285500478b69d
honor2016tw <honor2016tw@gmail.com>
2021-10-04 11:20:51 +0800

    10/24

---
+++ Sprout/36.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+	int T;
+	while(cin>>T){
+		stack<int> S;
+		while(T--){
+			int a,b;
+			while(cin>>a){
+				switch(a){
+					case 1:
+						cin>>b;
+						S.push(b);
+						break;
+					case 2:
+						if(S.empty()){
+							cout<<"empty!"<<endl;
+							break;
+						}
+						cout<<S.top()<<endl;
+						S.pop();
+				}
+			}
+		}
+	}
+}
