dd35f35055c8ed58a33b8d0b722c126b38b8bc5a
honor2016tw <honor2016tw@gmail.com>
2021-10-18 11:26:29 +0800

    10/18

---
+++ Sprout/208.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int m;
+  while(cin>>m){
+    switch((m+2)/3){
+      case 1:
+        cout<<"spring"<<endl;
+        break;
+      case 2:
+        cout<<"summer"<<endl;
+        break;
+      case 3:
+        cout<<"fall"<<endl;
+        break;
+      case 4:
+        cout<<"winter"<<endl;
+        break;
+    }
+  }
+}
