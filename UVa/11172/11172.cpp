d50b5b224c511546bfab9a4d038405bcbbbdeb2f
honor2016tw <honor2016tw@gmail.com>
2021-10-20 20:56:55 +0800

    10/20

---
+++ UVa/11172/11172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int t,a,b;
+  while(cin>>t){
+    while(t--){
+      cin>>a>>b;
+      if(a>b) cout<<">"<<endl;
+      else if(a<b) cout<<"<"<<endl;
+      else cout<<"="<<endl;
+    }
+  }
+}
