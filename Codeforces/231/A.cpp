a372a08c93b1c4816a6520c259bfbddbd51e912c
honor2016tw <honor2016tw@gmail.com>
2021-10-25 20:54:00 +0800

    10/25

---
+++ Codeforces/231/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n,a,b,c;
+  while(cin>>n){
+    int count=0;
+    while(n--){
+      cin>>a>>b>>c;
+      if(a+b+c>=2) count++;
+    }
+    cout<<count<<endl;
+  }
+}
