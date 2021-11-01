03abee09443954246ba54aee431e0881b2ed880d
honor2016tw <honor2016tw@gmail.com>
2021-11-01 11:02:45 +0800

    11/1

---
+++ Sprout/294.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int arr[10005];
+int main(){
+  int n;
+  while(cin>>n){
+    for(int i=0;i<n;i++) cin>>arr[i];
+    for(int i=n-1;i>=0;i--){
+      if(arr[i]>=0) cout<<0<<endl;
+      else cout<<arr[i]<<endl;
+    }
+  }
+}
