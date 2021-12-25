bdaf596a85c3b27096532239c75fa5135ff10625
honor2016tw <honor2016tw@gmail.com>
2021-12-25 22:40:45 +0800

    12/25

---
+++ Sprout/953.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string s,k;
+	int p[26];
+	for(int i=0;i<26;i++) p[i]=0;
+	cin>>s;
+	cin>>k;
+	for(int i=0;i<s.length();i++){
+		p[((s[i]+(k[i]-'a'))-97)%26]++;
+	}
+	for(int i=0;i<25;i++) cout<<p[i]<<" ";
+	cout<<p[25];
+}
