5befa3588131960a352e049a0ef84697972a94de
honor2016tw <honor2016tw@gmail.com>
2022-02-01 15:24:29 +0800

    2/1

---
+++ ZeroJudge/f044.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int GCD(int a,int b){
+  if(a==0) return b;
+  if(b==0) return a;
+  return GCD(b,a%b);
+}
+
+int main(){
+  int N,T;
+  while(cin>>N>>T){
+    int gcd=GCD(N,T);
+    N=N/gcd;
+    T=T/gcd;
+    cout<<log2(N+T)<<endl;
+  }
+}
