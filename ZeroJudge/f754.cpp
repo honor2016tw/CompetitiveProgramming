a6522b2017f4229f344cd891cfd1e32a6f75702a
honor2016tw <honor2016tw@gmail.com>
2022-02-01 15:52:19 +0800

    2/1-1

--- ZeroJudge/f754.cpp
+++ ZeroJudge/f754.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #define LL long long 
-#define fastio cin.tie(0);ios::sync_with_stdio(0);
+#define fastio cin.tie(0);cin.sync_with_stdio(0);
+#define endl "\n"
 using namespace std;
 
 int main(){
   fastio
-  LL int K,M;
+  LL int K,M,Q,R;
   int N;
     cin>>N;
     for(int i=1;i<=N;i++){
       cin>>M>>K;
+      Q=M/K;
+      R=M%K;
       cout<<"Case "<<i<<" :"<<endl;
       for(int j=1;j<K;j++){
-        cout<<"第"<<j<<"位 : 拿"<<M/K<<"元並說DD! BAD!"<<endl;
+        cout<<"第"<<j<<"位 : 拿"<<Q<<"元並說DD! BAD!"<<endl;
       }
-      cout<<"第"<<K<<"位 : 拿"<<M/K+(M%K)<<"元並說DD! BAD!"<<endl;
+      cout<<"第"<<K<<"位 : 拿"<<Q+R<<"元並說DD! BAD!"<<endl;
     }
   
 }
