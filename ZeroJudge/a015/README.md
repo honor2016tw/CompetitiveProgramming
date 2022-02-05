20670baae391cfd2a16c5897405450e7a877d15c
honor2016tw <honor2016tw@gmail.com>
2022-02-05 16:41:26 +0800

    2/5

---
+++ ZeroJudge/a015/README.md
@@ -0,0 +1,1 @@
+# [a015](https://zerojudge.tw/ShowProblem?problemid=a015)
---
+++ ZeroJudge/a015/a015.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#define loop for(int i=0;i<r;i++) for(int j=0;j<c;j++)
+using namespace std;
+
+int arr[101][101];
+int CP[101][101];
+
+int main(){
+  int r,c;
+  while(cin>>r>>c){
+    loop
+      cin>>arr[i][j];
+    loop
+      CP[j][i] = arr[i][j];
+    swap(r,c);
+    for(int i=0;i<r;i++){
+      for(int j=0;j<c;j++)
+        cout<<CP[i][j]<<" ";
+      cout<<endl;
+    }
+  }
+}
