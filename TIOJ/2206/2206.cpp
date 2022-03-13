831e906ebc76a462bd920d31e5e38e929fca918f
honor2016tw <honor2016tw@gmail.com>
2022-03-13 12:46:22 +0800

    3/13

--- TIOJ/2206/2206.cpp
+++ TIOJ/2206/2206.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #define MAXN 100005
-
+#define fastio cin.tie(0);ios::sync_with_stdio(0)
+#define endl "\n"
 using namespace std;
 
-int arr[MAXN];
+int arr[MAXN],times[MAXN];
 
 int RecQuery(int a,int b){
   if(b==1) return arr[a];
+  while(times[a]!=0 && b-1>times[a]){
+      b = b-times[a];
+    }
   return arr[RecQuery(a,b-1)];
 }
 
 int main(){
+  fastio;
   int N,Q,a,b;
   while(cin>>N){
     for(int i=1;i<=N;i++) cin>>arr[i];
+    for(int i=1;i<=N;i++){
+      int cnt=2;
+      while(true){
+        if(RecQuery(i,cnt) == arr[i])
+          break;
+        if(cnt >= N){
+            cnt=0;
+            break;
+          }
+        else
+          cnt++;
+      }
+      times[i] = cnt;
+    }
     cin>>Q;
     for(int i=0;i<Q;i++){
       cin>>a>>b;
       if(!b) 
         cout<<a<<endl;
-      else
-        cout<<RecQuery(a,b)<<endl;
+      else{
+       cout<<RecQuery(a,b)<<endl;
+       }
     }
   }
 }
