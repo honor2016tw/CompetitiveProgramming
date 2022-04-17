93abfbe525c94d589871a84f9aeafa4de0d37d7d
honor2016tw <honor2016tw@gmail.com>
2022-04-17 10:58:29 +0800

    4/17

--- TIOJ/2206/2206.cpp
+++ TIOJ/2206/2206.cpp
@@ -4,30 +4,37 @@
 #define endl "\n"
 using namespace std;
 
-int arr[MAXN],times[MAXN];
+int arr[MAXN],counting[MAXN],times[MAXN];
 
 int RecQuery(int a,int b){
+  if(times[a]!=0)
+    b=b%(times[a]-1);
+  if(b==0) return a;
   if(b==1) return arr[a];
-  while(times[a]!=0 && b-1>times[a]){
-      b = b-times[a];
-    }
   return arr[RecQuery(a,b-1)];
 }
 
 int main(){
-  fastio;
+  //fastio;
   int N,Q,a,b;
   while(cin>>N){
-    for(int i=1;i<=N;i++) cin>>arr[i];
+    for(int i=1;i<=N;i++) {
+      cin>>arr[i];
+      counting[arr[i]]++;
+     }
     for(int i=1;i<=N;i++){
+      if(counting[i]==0){
+        times[i]=0;
+        continue;
+      }
       int cnt=2;
       while(true){
+        if(cnt>=N){
+          cnt=0;
+          break;
+        }
         if(RecQuery(i,cnt) == arr[i])
           break;
-        if(cnt >= N){
-            cnt=0;
-            break;
-          }
         else
           cnt++;
       }
